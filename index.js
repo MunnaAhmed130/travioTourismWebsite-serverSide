207ed32917bb3855355f0b34cdd25a54df36ab93
MunnaAhmed130 <munnaahmed2025@gmail.com>
2021-10-29 03:05:29 +0600

    commmit

--- index.js
+++ index.js
@@ -14,6 +14,20 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+
+async function run() {
+    try {
+        await client.connect();
+
+        const database = client.db("serviceCollection");
+        const serviceCollection = database.collection("service")
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
     res.send('Travel related server');
 })
