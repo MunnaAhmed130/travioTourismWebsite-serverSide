b1965c7a1fe6c6da7bdac1d70748ec448723213c
Munna Ahmed <munnaahmed2025@gmail.com>
2023-03-02 00:18:47 +0600

    stop waiting for client to connect

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        await client.connect();
+        client.connect();
 
         const database = client.db("travelAgency");
         const planCollection = database.collection("tourPlan");
